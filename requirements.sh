a88b769e4db77e01c0628e69a1f77ade5bda2400
PhDSantaCruz <mcs.santacruz.ia@gmail.com>
2021-01-11 12:56:59 -0600

    Omar Castellanos Santa Cruz

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+echo "deb https://rundeck.bintray.com/rundeck-deb /" | sudo tee -a /etc/apt/sources.list.d/rundeck.list
+curl 'https://bintray.com/user/downloadSubjectPublicKey?username=bintray' | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install rundeck -y
+sudo service rundeckd start 
+tail -f /var/log/rundeck/service.log
